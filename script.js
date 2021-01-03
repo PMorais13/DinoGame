1fd7f1eb76032802886f2422cb00f5f2a2fb9a62
PMorais <phmscout@gmail.com>
2021-01-03 17:59:59 -0300

    adiciona aumento de velocidade para cada cactus passado

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-const dino = document.querySelector('.dino')
-const background = document.querySelector('.background')
-let isJumping = false
-let position = 0
+const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
+let playing = true;
+let isJumping = false;
+let position = 0;
 let audios = {
     jumping: 'audios/bipUp.wav',
     overAudio: 'audios/gameOver.wav'
 };
+let speed = 9
 
-function handleKeyUp(event) {   
+function handleKeyUp(event) {
 
     if (event.keyCode === 32 && !isJumping) {
         jump();
@@ -23,61 +25,67 @@ function playAudio(audioName) {
 }
 
 function jump() {
-    playAudio('jumping')
-    isJumping = true
+    playAudio('jumping');
+    isJumping = true;
 
-    let upInterval = setInterval (() => {
-        if (position >= 150){
-            clearInterval(upInterval)
+    let upInterval = setInterval(() => {
 
-            let downInterval = setInterval (() => {
+        if (position >= 150) {
+            clearInterval(upInterval);
+
+            let downInterval = setInterval(() => {
+
                 if (position <= 0) {
-                    clearInterval(downInterval)
-                    isJumping = false
+                    clearInterval(downInterval);
+                    isJumping = false;
                 } else {
-                    position -= 20
-                    dino.style.bottom = position +'px'
+                    position -= 20;
+                    dino.style.bottom = position + 'px';
                 }
-            }, 20)
+            }, 20);
 
         } else {
 
-            position += 20
-            dino.style.bottom = position +'px'
+            position += 20;
+            dino.style.bottom = position + 'px';
         }
-    }, 20)
+    }, 20);
 }
 
-function createCactus () {
-    let cactus = document.createElement ('div')
-    let cactusPosition = 1200
-    let randomTime = Math.random () * 6000
+function createCactus() {
+    const cactus = document.createElement('div');
+    let cactusPosition = 1200;
+    let randomTime = Math.random() * (5000 - Number(speed + '00'));
 
-    cactus.classList.add ('cactus')
-    cactus.style.left = 1200 + 'px'
-    background.appendChild(cactus)
+    cactus.classList.add('cactus');
+    cactus.style.left = 1200 + 'px';
+    background.appendChild(cactus);
 
-    let leftInterval = setInterval (() => {
-    
-     if (cactusPosition < -50) {
-        clearInterval(leftInterval)
-        background.removeChild(cactus)
-        
-     } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60){
-        // game over
-        playAudio('overAudio')
-        clearInterval(leftInterval)
-        document.body.innerHTML = '<h1 class=" game-over"> Fim de jogo</h1>'
-    
-     } else {
-        cactusPosition -= 10
-        cactus.style.left = cactusPosition + 'px'
-     }
-    }, 20)
+    let leftInterval = setInterval(() => {
 
-    setTimeout (createCactus, randomTime)
+        if (cactusPosition < -50) {
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+
+        } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+            // game over
+
+            clearInterval(leftInterval);
+            document.body.innerHTML = '<h1 class="game-over"> Fim de jogo</h1>';
+            playing = false;
+
+        } else {
+            cactusPosition -= speed;
+            cactus.style.left = cactusPosition + 'px';
+        }
+    }, 20);
+
+    if (playing) {
+        setTimeout(createCactus, randomTime);
+        speed ++
+    }
 }
 
-createCactus()
-document.addEventListener('keydown', handleKeyUp)
+createCactus();
+document.addEventListener('keydown', handleKeyUp);
 
